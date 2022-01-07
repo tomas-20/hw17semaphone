d1247d9e53cf5fd863dac1f36cc44d62b3919818
tomas-20 <tacuna20@stuy.edu>
2022-01-07 00:02:20 -0500

    created write.c

---
+++ write.c
@@ -0,0 +1,14 @@
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+  int shmid = shmget(2187, sizeof (char[10000]), 0);
+  char *shm = shmat(shmid, NULL, 0);
+  printf("the last line was:\n%syou are adding:\n", shm);
+  fgets(shm, 10000, stdin);
+  shmdt(shm);
+  return 0;
+}

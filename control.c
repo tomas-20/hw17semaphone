788b27e6544354f1da01c7edba5f2708287be2de
tomas-20 <tacuna20@stuy.edu>
2022-01-08 19:35:48 -0500

    made some more helper functions in control.c

--- control.c
+++ control.c
@@ -7,15 +7,30 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+void create_shm_string(int key, int length) {
+  int shmid = shmget(key, sizeof (char[length + 1]), IPC_CREAT | IPC_RMID | 0644);
+  char *shm = shmat(shmid, NULL, 0);
+  *shm = '\0';
+  shmdt(shm);
+  printf("created shared memory %d\n", shmid);
+}
+
 void create_file(char *path) {
   int file = open(path, O_CREAT | O_TRUNC, 0644);
   close(file);
+  printf("created %s\n", path);
 }
 
 int get_file_size(char *path) {
   struct stat file_stats;
   stat(path, &file_stats);
   return file_stats.st_size;
+}
+
+void remove_shm(int key) {
+  int shmid = shmget(key, 0, 0);
+  shmctl(shmid, IPC_RMID, 0);
+  printf("removed shared memory %d\n", shmid);
 }
 
 void print_file(char *path) {
@@ -25,25 +40,17 @@ void print_file(char *path) {
   int file = open(path, O_RDONLY);
   read(file, string, file_size);
   close(file);
-  printf("%s", string);
+  printf("contents of %s:\n%s", path, string);
 }
 
 int main(int argc, char **argv) {
   if (argc >= 2) {
     if (!strcmp(argv[1], "create")) {
-      int shmid = shmget(2187, sizeof (char[10000]), IPC_CREAT | IPC_RMID | 0644);
-      printf("created shared memory %d\n", shmid);
-      char *shm = shmat(shmid, NULL, 0);
-      *shm = '\0';
-      shmdt(shm);
+      create_shm_string(2187, 10000);
       create_file("story.txt");
-      printf("created story.txt\n");
     }
     else if (!strcmp(argv[1], "remove")) {
-      int shmid = shmget(2187, 0, 0);
-      shmctl(shmid, IPC_RMID, 0);
-      printf("removed shared memory %d\n", shmid);
-      printf("contents of story:\n");
+      remove_shm(2187);
       print_file("story.txt");
     }
   }

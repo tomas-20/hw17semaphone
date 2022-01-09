7ab340d57426019628770b4f75faa4a88a01e4c5
tomas-20 <tacuna20@stuy.edu>
2022-01-08 20:00:12 -0500

    made some helper functions in write.c

--- write.c
+++ write.c
@@ -6,14 +6,25 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
-  int shmid = shmget(2187, 0, 0);
-  char *shm = shmat(shmid, NULL, 0);
-  printf("the last line was:\n%syou are adding:\n", shm);
-  fgets(shm, 10000, stdin);
-  int file = open("story.txt", O_WRONLY | O_APPEND);
-  dprintf(file, "%s", shm);
+char *get_shm_string(int key) {
+  return shmat(shmget(key, 0, 0), NULL, 0);
+}
+
+void fill_with_input(char *string, int length) {
+  fgets(string, length + 1, stdin);
+}
+
+void write_to_file(char *path, char *string) {
+  int file = open(path, O_WRONLY | O_APPEND);
+  dprintf(file, "%s", string);
   close(file);
+}
+
+int main() {
+  char *shm = get_shm_string(2187);
+  printf("the last line was:\n%syou are adding:\n", shm);
+  fill_with_input(shm, 10000);
+  write_to_file("story.txt", shm);
   shmdt(shm);
   return 0;
 }

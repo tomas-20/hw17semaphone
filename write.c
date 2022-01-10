06286d00b8db83bc5fb8421935b60782f7032b30
tomas-20 <tacuna20@stuy.edu>
2022-01-09 23:22:51 -0500

    made it so that the semaphore is downed and upped to use the shared memory and file

--- write.c
+++ write.c
@@ -1,10 +1,23 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
+#include <sys/sem.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+
+int get_semid(int key) {
+  return semget(key, 1, 0);
+}
+
+void change_sem(int semid, int amount) {
+  struct sembuf sb;
+  sb.sem_num = 0;
+  sb.sem_flg = SEM_UNDO;
+  sb.sem_op = amount;
+  semop(semid, &sb, 1);
+}
 
 char *get_shm_string(int key) {
   return shmat(shmget(key, 0, 0), NULL, 0);
@@ -21,10 +34,13 @@ void write_to_file(char *path, char *string) {
 }
 
 int main() {
+  int semid = get_semid(24601);
+  change_sem(semid, -1);
   char *shm = get_shm_string(2187);
   printf("the last line was:\n%syou are adding:\n", shm);
   fill_with_input(shm, 10000);
   write_to_file("story.txt", shm);
   shmdt(shm);
+  change_sem(semid, 1);
   return 0;
 }
